459da67b1865090d04d21cab8cb2d9943c7804a6
keane-kane <abdouazizk50@gmail.com>
2020-07-06 18:37:17 +0200

    var

--- src/Controller/BatimentController.php
+++ src/Controller/BatimentController.php
@@ -12,26 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BatimentController extends AbstractController
 {
-    /**
-     * @Route("/batiment", name="batiment")
-     */
- 
-    public function index(Request $request):Response
-    {
-        $batiment = new Batiment();
-        $form = $this->createForm(BatimentType::class,$batiment);
-        // dd($batiment);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($batiment);
-            $em->flush();
-        }
-        return $this->render('batiment/index.html.twig', [
-             'form'=> $form->createView(),
-            
-        ]);
-    }
      /**
      * @Route("/batiment/afficher", name="batiment_afficher")
     */
@@ -41,7 +21,7 @@ class BatimentController extends AbstractController
         // dd($batiments);
         return $this->render('batiment/affiche_bat.html.twig', compact('batiments'));
     }
-  
+    
     /**
      * @Route("/batiment/{id<[0-9]+>}", name="batiment_read")
     */
@@ -49,4 +29,28 @@ class BatimentController extends AbstractController
     {
         return $this->render('batiment/read.html.twig', compact('batiment'));
     }
+
+    /**
+     * @Route("/batiment", name="batiment")
+     */
+ 
+    public function index(Request $request):Response
+    {
+        
+        $batiment = new Batiment();
+        $form = $this->createForm(BatimentType::class,$batiment);
+        // dd($batiment);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($batiment);
+            $em->flush();
+        }
+        
+        return $this->render('batiment/index.html.twig', [
+             'form'=> $form->createView(),
+            
+        ]);
+    }
+   
 }
